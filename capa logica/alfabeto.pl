48152151f1359280442e50c6642a1a568dbf4a1c
carolina <carolina.torres.baroffio@gmail.com>
2026-06-04 17:16:04 -0300

    alfabetos en ingles y español para los cifradosl

---
+++ capa logica/alfabeto.pl
@@ -0,0 +1,15 @@
+
+alfabeto(26, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
+alfabeto(27, [a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z]).
+
+modulo_valido(26).
+modulo_valido(27).
+
+
+pos_en_alfabeto(Char, Alfabeto, Pos) :-
+    nth0(Pos, Alfabeto, Char), !. %    El corte evita buscar posiciones duplicadas.
+
+
+char_en_alfabeto(Char, Modulo) :-   %    Verifica si un carácter pertenece al alfabeto del módulo.
+    alfabeto(Modulo, Alfa),
+    member(Char, Alfa).
