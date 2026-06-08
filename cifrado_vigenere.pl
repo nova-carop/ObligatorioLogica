83b0a8e5a0e063799879b4afde7571890144c0fa
Joaquín Cabrera <joaco_cab@MacBook-Pro-de-Joaquin-4.local>
2026-06-08 18:08:44 -0300

    Viggdaddynere

--- cifrado_vigenere.pl
+++ cifrado_vigenere.pl
@@ -1,37 +1,59 @@
-letra_num(Letra, Numero) :- char_code(Letra, Codigo) , Numero is Codigo - 65.
-
-num_letra(Numero, Letra) :- Codigo is Numero + 65 , char_code(Letra, Codigo).
+:- encoding(utf8).
+:- consult(alfabeto).
 
 extender_clave(_, 0, []) :- !.
-extender_clave(Clave, N, [K|Resto]) :-
-    N > 0,
-    length(Clave, Len),
-    Idx is (N - 1) mod Len,
-    nth0(Idx, Clave, K),
-    N1 is N - 1,
-    extender_clave(Clave, N1, Resto).
+extender_clave(Clave, Largo, [LetraClave|Resto]) :-
+    Largo > 0,
+    length(Clave, LongClave),
+    Indice is (Largo - 1) mod LongClave,
+    nth0(Indice, Clave, LetraClave),
+    LargoMenos1 is Largo - 1,
+    extender_clave(Clave, LargoMenos1, Resto).
 
-cifrar_letras([], [], []).
-cifrar_letras([M|Ms], [K|Ks], [C|Cs]) :-
-    letra_num(M, NM),
-    letra_num(K, NK),
-    NC is (NM + NK) mod 26,
-    num_letra(NC, C),
-    cifrar_letras(Ms, Ks, Cs).
-  
-descifrar_letras([], [], []).
-descifrar_letras([C|Cs], [K|Ks], [M|Ms]) :-
-    letra_num(C, NC),
-    letra_num(K, NK),
-    NM is (NC - NK + 26) mod 26,
-    num_letra(NM, M),
-    descifrar_letras(Cs, Ks, Ms).
+clave_extendida(Clave, Largo, ClaveExtendida) :-
+    extender_clave(Clave, Largo, ClaveAlReves),
+    reverse(ClaveAlReves, ClaveExtendida).
 
-  %Predicado Principal
-  cifrar(Mensaje, Clave, Cifrado) :-
-    upcase_atom(Mensaje, MU), atom_chars(MU, LM),
-    upcase_atom(Clave, CU),   atom_chars(CU, LC),
-    length(LM, Largo),
-    clave_extendida(LC, Largo, LCext),
-    cifrar_letras(LM, LCext, LCif),
-    atom_chars(Cifrado, LCif).
+cifrar_char(Char, LetraClave, Idioma, CharCifrado) :-
+    alfabeto(Idioma, Alfa),
+    idioma_modulo(Idioma, Modulo),
+    pos_en_alfabeto(Char, Alfa, PosChar),
+    pos_en_alfabeto(LetraClave, Alfa, PosClave), !,
+    NuevaPos is (PosChar + PosClave) mod Modulo,
+    nth0(NuevaPos, Alfa, CharCifrado).
+cifrar_char(Char, _, _, Char).
+
+descifrar_char(Char, LetraClave, Idioma, CharOriginal) :-
+    alfabeto(Idioma, Alfa),
+    idioma_modulo(Idioma, Modulo),
+    pos_en_alfabeto(Char, Alfa, PosChar),
+    pos_en_alfabeto(LetraClave, Alfa, PosClave), !,
+    NuevaPos is ((PosChar - PosClave) mod Modulo + Modulo) mod Modulo,
+    nth0(NuevaPos, Alfa, CharOriginal).
+descifrar_char(Char, _, _, Char).
+
+cifrar_lista([], _, _, []).
+cifrar_lista([H|T], [K|Ks], Idioma, [HC|TC]) :-
+    cifrar_char(H, K, Idioma, HC),
+    cifrar_lista(T, Ks, Idioma, TC).
+
+descifrar_lista([], _, _, []).
+descifrar_lista([H|T], [K|Ks], Idioma, [HC|TC]) :-
+    descifrar_char(H, K, Idioma, HC),
+    descifrar_lista(T, Ks, Idioma, TC).
+
+cifrar(Mensaje, Clave, Idioma, Cifrado) :-
+    downcase_atom(Mensaje, M), atom_chars(M, ListaMensaje),
+    downcase_atom(Clave, C),   atom_chars(C, ListaClave),
+    length(ListaMensaje, Largo),
+    clave_extendida(ListaClave, Largo, ClaveExtendida),
+    cifrar_lista(ListaMensaje, ClaveExtendida, Idioma, ListaCifrada),
+    atomic_list_concat(ListaCifrada, Cifrado).
+
+descifrar(Cifrado, Clave, Idioma, Mensaje) :-
+    downcase_atom(Cifrado, C), atom_chars(C, ListaCifrada),
+    downcase_atom(Clave, K),   atom_chars(K, ListaClave),
+    length(ListaCifrada, Largo),
+    clave_extendida(ListaClave, Largo, ClaveExtendida),
+    descifrar_lista(ListaCifrada, ClaveExtendida, Idioma, ListaMensaje),
+    atomic_list_concat(ListaMensaje, Mensaje).
