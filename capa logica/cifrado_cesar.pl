6453c956cfdece1e30f13b6ec0f664af6d44171f
carolina <carolina.torres.baroffio@gmail.com>
2026-06-17 10:12:43 -0300

    codigo emprolijado funcional final

--- capa logica/cifrado_cesar.pl
+++ capa logica/cifrado_cesar.pl
@@ -1,9 +1,6 @@
 :- encoding(utf8).
 :- consult(alfabeto).
 
-% Desplazamiento fijo para la demo educativa
-desp_fijo(3).
-
 
 desplazar(Pos, Desp, Modulo, NuevaPos) :-
     NuevaPos is ((Pos + Desp) mod Modulo + Modulo) mod Modulo.
@@ -32,92 +29,3 @@ descifrar_lista(Lista, Desp, Idioma, Descifrado) :-
     cifrar_lista(Lista, DespNegativo, Idioma, Descifrado).
 
 
-
-%  MENÚ DE PRUEBA  (será reemplazado por frontend Python)
-
-menu :-
-    nl,
-    write('=== CIFRADO CÉSAR ==='), nl,
-    write('1. Cifrar mensaje'), nl,
-    write('2. Descifrar mensaje'), nl,
-    write('3. Salir'), nl,
-    write('Opcion: '),
-    read(Opcion),
-    manejar_opcion(Opcion).
-
-
-
-leer_idioma(Idioma) :-
-    write('Idioma (espanol/ingles): '),
-    read(Idioma),
-    ( idioma_modulo(Idioma, _) -> true
-    ; write('Idioma invalido. Usa espanol o ingles.'), nl, leer_idioma(Idioma)
-    ).
-
-
-leer_desplazamiento(Desp) :-
-    nl,
-    write('Tipo de desplazamiento:'), nl,
-    write('  1. Fijo (3) - para ver como funciona el cifrado'), nl,
-    write('  2. Personalizado'), nl,
-    write('Opcion: '),
-    read(TipoDesp),
-    resolver_desp(TipoDesp, Desp).
-
-resolver_desp(1, Desp) :- !,
-    desp_fijo(Desp),
-    write('Desplazamiento fijo: '), write(Desp), nl.
-
-resolver_desp(2, Desp) :- !,
-    write('Ingrese el desplazamiento: '),
-    read(Desp),
-    ( integer(Desp) -> true
-    ; write('Debe ser un numero entero.'), nl, fail
-    ).
-
-resolver_desp(_, Desp) :-
-    write('Opcion invalida.'), nl,
-    leer_desplazamiento(Desp).
-
-
-% --- Convierte átomo a lista de chars en minúsculas ---
-leer_mensaje(Lista) :-
-    write('Mensaje (ej: hola): '),
-    read(Atom),
-    downcase_atom(Atom, Lower),
-    atom_chars(Lower, Lista).
-
-% --- Convierte lista de chars a átomo para mostrar ---
-mostrar_resultado(Lista) :-
-    atomic_list_concat(Lista, Resultado),
-    write(Resultado).
-
-
-manejar_opcion(1) :-
-    write('--- CIFRAR ---'), nl,
-    leer_mensaje(Lista),
-    leer_idioma(Idioma),
-    leer_desplazamiento(Desp),
-    cifrar_lista(Lista, Desp, Idioma, Cifrado),
-    nl, write('Resultado: '), mostrar_resultado(Cifrado), nl,
-    menu.
-
-manejar_opcion(2) :-
-    write('--- DESCIFRAR ---'), nl,
-    leer_mensaje(Lista),
-    leer_idioma(Idioma),
-    leer_desplazamiento(Desp),
-    descifrar_lista(Lista, Desp, Idioma, Descifrado),
-    nl, write('Resultado: '), mostrar_resultado(Descifrado), nl,
-    menu.
-
-
-
-
-manejar_opcion(3) :-
-    write('Hasta luego.'), nl.
-
-
-manejar_opcion(_) :-
-    write('Opcion invalida.'), nl,
-    menu.
